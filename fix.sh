0a6d141e96e0266a97f677e96631967c0e53d072
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-05-23 20:24:49 +0000

    fix all article pages

--- fix.sh
+++ fix.sh
@@ -2,58 +2,82 @@
 # the article.html
 # files across all folders in articles folder
 
-mkdir articles
-mkdir articles/nutrition
-mkdir articles/workouts
-mkdir articles/sports
-mkdir articles/technology
-mkdir articles/fashion
-mkdir articles/travel
-mkdir articles/mental-health
-mkdir articles/lifestyle
+rm -rf articles/nutrition/article1.html
+rm -rf articles/nutrition/article2.html
+rm -rf articles/nutrition/article3.html
+rm -rf articles/nutrition/article4.html
+rm -rf articles/nutrition/article5.html
+rm -rf articles/workouts/article1.html
+rm -rf articles/workouts/article2.html
+rm -rf articles/workouts/article3.html
+rm -rf articles/workouts/article4.html
+rm -rf articles/workouts/article5.html
+rm -rf articles/sports/article1.html
+rm -rf articles/sports/article2.html
+rm -rf articles/sports/article3.html
+rm -rf articles/sports/article4.html
+rm -rf articles/sports/article5.html
+rm -rf articles/technology/article1.html
+rm -rf articles/technology/article2.html
+rm -rf articles/technology/article3.html
+rm -rf articles/technology/article4.html
+rm -rf articles/technology/article5.html
+rm -rf articles/fashion/article1.html
+rm -rf articles/fashion/article2.html
+rm -rf articles/fashion/article3.html
+rm -rf articles/fashion/article4.html
+rm -rf articles/fashion/article5.html
+rm -rf articles/travel/article1.html
+rm -rf articles/travel/article2.html
+rm -rf articles/travel/article3.html
+rm -rf articles/travel/article4.html
+rm -rf articles/travel/article5.html
+rm -rf articles/mental-health/article1.html
+rm -rf articles/mental-health/article2.html
+rm -rf articles/mental-health/article3.html
+rm -rf articles/mental-health/article4.html
+rm -rf articles/mental-health/article5.html
+rm -rf articles/lifestyle/article1.html
+rm -rf articles/lifestyle/article2.html
+rm -rf articles/lifestyle/article3.html
+rm -rf articles/lifestyle/article4.html
+rm -rf articles/lifestyle/article5.html
 
 cp article.html articles/nutrition/article1.html
 cp article.html articles/nutrition/article2.html
 cp article.html articles/nutrition/article3.html
 cp article.html articles/nutrition/article4.html
 cp article.html articles/nutrition/article5.html
-
 cp article.html articles/workouts/article1.html
 cp article.html articles/workouts/article2.html
 cp article.html articles/workouts/article3.html
 cp article.html articles/workouts/article4.html
 cp article.html articles/workouts/article5.html
-
 cp article.html articles/sports/article1.html
 cp article.html articles/sports/article2.html
 cp article.html articles/sports/article3.html
 cp article.html articles/sports/article4.html
 cp article.html articles/sports/article5.html
-
 cp article.html articles/technology/article1.html
 cp article.html articles/technology/article2.html
 cp article.html articles/technology/article3.html
 cp article.html articles/technology/article4.html
 cp article.html articles/technology/article5.html
-
 cp article.html articles/fashion/article1.html
 cp article.html articles/fashion/article2.html
 cp article.html articles/fashion/article3.html
 cp article.html articles/fashion/article4.html
 cp article.html articles/fashion/article5.html
-
 cp article.html articles/travel/article1.html
 cp article.html articles/travel/article2.html
 cp article.html articles/travel/article3.html
 cp article.html articles/travel/article4.html
 cp article.html articles/travel/article5.html
-
 cp article.html articles/mental-health/article1.html
 cp article.html articles/mental-health/article2.html
 cp article.html articles/mental-health/article3.html
 cp article.html articles/mental-health/article4.html
 cp article.html articles/mental-health/article5.html
-
 cp article.html articles/lifestyle/article1.html
 cp article.html articles/lifestyle/article2.html
 cp article.html articles/lifestyle/article3.html
