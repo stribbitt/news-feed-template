c6f88f831f021be0f6db18e8f9d3ebcfb669ad08
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-05-23 20:17:34 +0000

    build out folder structure

---
+++ fix.sh
@@ -0,0 +1,61 @@
+# only run this if you need to update 
+# the article.html
+# files across all folders in articles folder
+
+mkdir articles
+mkdir articles/nutrition
+mkdir articles/workouts
+mkdir articles/sports
+mkdir articles/technology
+mkdir articles/fashion
+mkdir articles/travel
+mkdir articles/mental-health
+mkdir articles/lifestyle
+
+cp article.html articles/nutrition/article1.html
+cp article.html articles/nutrition/article2.html
+cp article.html articles/nutrition/article3.html
+cp article.html articles/nutrition/article4.html
+cp article.html articles/nutrition/article5.html
+
+cp article.html articles/workouts/article1.html
+cp article.html articles/workouts/article2.html
+cp article.html articles/workouts/article3.html
+cp article.html articles/workouts/article4.html
+cp article.html articles/workouts/article5.html
+
+cp article.html articles/sports/article1.html
+cp article.html articles/sports/article2.html
+cp article.html articles/sports/article3.html
+cp article.html articles/sports/article4.html
+cp article.html articles/sports/article5.html
+
+cp article.html articles/technology/article1.html
+cp article.html articles/technology/article2.html
+cp article.html articles/technology/article3.html
+cp article.html articles/technology/article4.html
+cp article.html articles/technology/article5.html
+
+cp article.html articles/fashion/article1.html
+cp article.html articles/fashion/article2.html
+cp article.html articles/fashion/article3.html
+cp article.html articles/fashion/article4.html
+cp article.html articles/fashion/article5.html
+
+cp article.html articles/travel/article1.html
+cp article.html articles/travel/article2.html
+cp article.html articles/travel/article3.html
+cp article.html articles/travel/article4.html
+cp article.html articles/travel/article5.html
+
+cp article.html articles/mental-health/article1.html
+cp article.html articles/mental-health/article2.html
+cp article.html articles/mental-health/article3.html
+cp article.html articles/mental-health/article4.html
+cp article.html articles/mental-health/article5.html
+
+cp article.html articles/lifestyle/article1.html
+cp article.html articles/lifestyle/article2.html
+cp article.html articles/lifestyle/article3.html
+cp article.html articles/lifestyle/article4.html
+cp article.html articles/lifestyle/article5.html
