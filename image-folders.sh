a7a70362e462d977c15cedb9173c9ea4ca3b037a
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-05-24 12:27:18 +0000

    added folders

---
+++ image-folders.sh
@@ -0,0 +1,8 @@
+mkdir articles/nutrition/images
+mkdir articles/workouts/images
+mkdir articles/sports/images
+mkdir articles/technology/images
+mkdir articles/fashion/images
+mkdir articles/travel/images
+mkdir articles/mental-health/images
+mkdir articles/lifestyle/images
