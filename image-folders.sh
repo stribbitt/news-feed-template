45f7ce41fe43189da1db7a5dc438cf9a17b401bb
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-05-24 12:40:55 +0000

    added readmes

--- image-folders.sh
+++ image-folders.sh
@@ -1,8 +1,17 @@
-mkdir articles/nutrition/images
-mkdir articles/workouts/images
-mkdir articles/sports/images
-mkdir articles/technology/images
-mkdir articles/fashion/images
-mkdir articles/travel/images
-mkdir articles/mental-health/images
-mkdir articles/lifestyle/images
+# mkdir articles/nutrition/images
+# mkdir articles/workouts/images
+# mkdir articles/sports/images
+# mkdir articles/technology/images
+# mkdir articles/fashion/images
+# mkdir articles/travel/images
+# mkdir articles/mental-health/images
+# mkdir articles/lifestyle/images
+
+touch articles/nutrition/images/README.txt
+touch articles/workouts/images/README.txt
+touch articles/sports/images/README.txt
+touch articles/technology/images/README.txt
+touch articles/fashion/images/README.txt
+touch articles/travel/images/README.txt
+touch articles/mental-health/images/README.txt
+touch articles/lifestyle/images/README.txt
